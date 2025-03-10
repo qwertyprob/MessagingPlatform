dd68e39cfe39adfd187b2e895f1ab01f3472bbdd
Arsenii Arlech1no <arsenii.subenok@iis.utm.md>
2025-03-10 14:39:16 +0200

    Add Resources(multi-language Pages)

---
+++ MessagingProject/Models/RefreshTokenResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MessagingProject.Models
+{
+    public class RefreshTokenResponse
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+
+        public bool IsSuccess => ErrorCode == 0 && !string.IsNullOrEmpty(Token);
+    }
+
+}
