bad5c772827bb276cb01eb495367afe976611629
Arsenii Arlech1no <arsenii.subenok@iis.utm.md>
2025-05-01 23:02:34 +0300

    Add ScreenLock page; Profile Info page

--- MessagingProject/Controllers/Profile/ProfileController.cs
+++ MessagingProject/Controllers/Profile/ProfileController.cs
@@ -7,18 +7,19 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Runtime.InteropServices;
 
 namespace MessagingProject.Controllers.Profile
 {
     [Authorize]
-    public class ProfileController : Controller
+    public class ProfileController : BaseController
     {
         private readonly IAuthService _authService;
         private readonly IUserService _userService;
         private readonly IValidator<LoginViewModel> _validator;
 
-        public ProfileController(IAuthService authService, IValidator<LoginViewModel> validator, IUserService userService) 
+        public ProfileController(IAuthService authService, IValidator<LoginViewModel> validator, IUserService userService):base(authService) 
         {
             _authService = authService;
             _validator = validator;
@@ -38,72 +39,157 @@ namespace MessagingProject.Controllers.Profile
         [Route("/Profile")]
         public IActionResult ProfileInfo()
         {
-            var ClaimDict = GetUserClaims();
+            var ClaimDict = GetUserClaimsFiltered();
 
+            if(ClaimDict is null)
+            {
+                return View(new Dictionary<string, string>());
+            }
             return View(ClaimDict);
         }
+
         [AllowAnonymous]
         [Route("/ScreenLock")]
-
         public IActionResult ScreenLock()
         {
+
+          HttpContext.SignOutAsync();
+
+
+
+
+
             var claims = GetUserClaims();
 
-            HttpContext.Session.SetString("Email", claims["Email"]);
-            HttpContext.Session.SetString("FullName", claims["FullName"]); 
 
-            HttpContext.SignOutAsync();
+            ViewBag.Claims = claims;
+
+
+            HttpContext.Session.SetString("Email", claims["Email"]);
+
 
             ViewBag.Email = HttpContext.Session.GetString("Email");
-            ViewBag.FullName = HttpContext.Session.GetString("FullName");
+
 
             return View();
+
+
         }
+
+
+
 
 
         [AllowAnonymous]
         [HttpPost]
         [Route("/ScreenLock")]
         public async Task<IActionResult> LoginUser([FromForm] LoginViewModel model)
+
+
         {
+
+
             try
+
+
             {
+
+
                 var validationResult = await _validator.ValidateAsync(model);
+
+
                 if (validationResult.IsValid)
+
+
                 {
+
+
                     var authResponse = await _authService.AuthenticateUserAsync(model);
+
+
                     await _authService.SignInUserAsync(authResponse, model);
+
+
                     return Redirect("/");
 
+
+
+
+
                 }
+
+
                 ViewBag.Claims = GetUserClaims();
+
+
                 var claims = GetUserClaims();
+
+
                 HttpContext.Session.SetString("Email", claims["Email"]);
+
+
                 validationResult.AddToModelState(ModelState);
+
+
+
+
+
+
 
 
                 return View("ScreenLock", model);
 
 
+
+
+
+
+
+
             }
+
+
             catch (UnauthorizedAccessException)
+
+
             {
+
+
                 var errorMessage = $"* {Resources.ValidateResource.Invalid}";
+
+
                 ViewData.ModelState.AddModelError("Email", errorMessage);
+
+
                 return View("Index", model);
+
+
             }
+
+
             catch (Exception ex)
+
+
             {
+
+
                 return StatusCode(500, $"Internal server error: {ex.Message}");
+
+
             }
+
+
         }
+
+
+
         private Dictionary<string, string> GetUserClaims()
         {
             return User.Claims.ToDictionary(c => c.Type, c => c.Value);
         }
 
         [AllowAnonymous]
-        private IDictionary<string,string> GetUserClaimss()
+        private IDictionary<string,string> GetUserClaimsFiltered()
         {
             var claims = HttpContext.User.Claims;
 
